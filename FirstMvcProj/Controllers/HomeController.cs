5c204774d2fd9864ea55588523990ae10801b569
RakeshK <rakesh.k@nalashaa.com>
2020-10-15 21:04:58 +0530

    First Commit

---
+++ FirstMvcProj/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMvcProj.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return Content("Success !!! Web Apllication is Up and Running with jenkins in place");
+        }
+    }
+}
