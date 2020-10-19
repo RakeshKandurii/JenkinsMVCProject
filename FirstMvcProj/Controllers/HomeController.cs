ef09b9f663aa2a6e5f3e54a4683dc7bfdfe08f79
RakeshK <rakesh.k@nalashaa.com>
2020-10-19 19:03:05 +0530

    #Second Commit

--- FirstMvcProj/Controllers/HomeController.cs
+++ FirstMvcProj/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace FirstMvcProj.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return Content("Success !!! Web Apllication is Up and Running with jenkins in place");
+            return Content("Success !!! Web Apllication is Up and Running with jenkins CI And CD in place");
         }
     }
 }
